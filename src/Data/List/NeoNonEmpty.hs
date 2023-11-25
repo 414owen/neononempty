5c1b9e2bd783c0798c50335f7b3fff65d637d705
Owen Shepherd <owen@owen.cafe>
2023-11-25 17:55:27 +0100

    feat: More group functions, more haddock

--- src/Data/List/NeoNonEmpty.hs
+++ src/Data/List/NeoNonEmpty.hs
@@ -25,7 +25,10 @@ Added functions:
   * `scanl'`
   * 'sortOn'
   * 'fromCons'
-
+  * 'groupAll'
+  * 'groupAllBy'
+  * 'groupAll1'
+  * 'groupAllBy1'
 
 Removed functions:
 
@@ -107,11 +110,15 @@ module Data.List.NeoNonEmpty
   , filter
   , partition
   , group
+  , groupAll
   , groupBy
+  , groupAllBy
   , groupWith
   , groupAllWith
   , group1
+  , groupAll1
   , groupBy1
+  , groupAllBy1
   , groupWith1
   , groupAllWith1
 
@@ -140,7 +147,7 @@ import Prelude
   , Maybe(..)
   , Monad(..)
   , Ord(..)
-  , Ordering
+  , Ordering(..)
   , Semigroup
   , Show(..)
   , ($)
@@ -316,148 +323,327 @@ sort = onNonEmpty NE.sort
 reverse :: NonEmpty a -> NonEmpty a
 reverse = onNonEmpty NE.reverse
 
--- | Produces all the prefixes of a nonempty stream, starting with the shortest.
+-- | Produces all the prefixes of a stream, starting with the shortest.
 -- The result is 'NonEmpty' because the result always contains the empty list as
 -- the first element.
 --
 -- >>> inits [1,2,3] 
 -- [[], [1], [1,2], [1,2,3]]
+--
 -- >>> inits [1]
 -- [[], [1]]
+--
 -- >>> inits []
 -- [[]]
 inits :: Foldable f => f a -> NonEmpty [a] 
 inits = unsafeFromList . List.inits . Foldable.toList
 
--- | 
+-- | Produces all the nonempty prefixes of a nonempty stream, starting with the shortest.
+--
+-- >>> inits1 [1,2,3]
+-- [[1], [1,2], [1,2,3]]
+--
+-- >>> inits1 [1]
+-- [[1]]
 inits1 :: forall a. NonEmpty a -> NonEmpty (NonEmpty a) 
 inits1 = onUnderlying @_ @(NE.NonEmpty (NE.NonEmpty a)) NE.inits1
 
+-- | Produces all the suffixes of a stream, starting with the longest.
+-- The result is 'NonEmpty' because the result always contains the empty list as
+-- the first element.
+--
+-- >>> tails [1,2,3]
+-- [[1, 2, 3], [2, 3], [3], []]
+--
+-- >>> tails [1]
+-- [[1], []]
+--
+-- >>> tails []
+-- [[]]
 tails :: Foldable f => f a -> NonEmpty [a]
 tails = fromNonEmpty . NE.tails
 
+-- | Produces all the nonempty suffixes of a nonempty stream, starting with the longest.
+--
+-- >>> tails1 [1,2,3]
+-- [[1, 2, 3], [2, 3], [3]]
+--
+-- >>> tails1 [1]
+-- [[1]]
 tails1 :: forall a. NonEmpty a -> NonEmpty (NonEmpty a) 
 tails1 = onUnderlying @_ @(NE.NonEmpty (NE.NonEmpty a)) NE.tails1
 
--- base-compat added these a little late.
+-- base-compat added `append`, `appendList`, and `prependList`
+
+-- | A monomorphic version of <> for 'NonEmpty'.
+--
+-- >>> append [1] [2, 3]
+-- [1, 2, 3]
 append :: NonEmpty a -> NonEmpty a -> NonEmpty a 
-append (NonEmpty (x NE.:| xs)) (NonEmpty (y NE.:| ys)) = NonEmpty $ x NE.:| (xs ++ y : ys)
+append (NonEmpty (x NE.:| xs)) (NonEmpty (y NE.:| ys))
+  = NonEmpty $ x NE.:| (xs ++ y : ys)
 
+-- | Append a list at the end of a 'NonEmpty'.
+--
+-- >>> appendList [1, 2, 3] []
+-- [1, 2, 3]
+--
+-- >>> appendList [1, 2, 3] [4, 5]
+-- [1, 2, 3, 4, 5]
 appendList :: NonEmpty a -> [a] -> NonEmpty a
-appendList (NonEmpty (x NE.:| xs)) ys = NonEmpty $ x NE.:| (xs ++ ys)
+appendList (NonEmpty (x NE.:| xs)) ys
+  = NonEmpty $ x NE.:| (xs ++ ys)
 
+-- | Prepend a list to the front of a 'NonEmpty'.
+--
+-- >>> prependList [] [1, 2, 3]
+-- [1, 2, 3]
+--
+-- >>> prependList [negate 1, 0] [1, 2, 3]
+-- [-1, 0, 1, 2, 3]
 prependList :: [a] -> NonEmpty a -> NonEmpty a
 prependList [] xs = xs
-prependList (x : xs) (NonEmpty (y NE.:| ys)) = NonEmpty $ x NE.:| (xs ++ y : ys)
+prependList (x : xs) (NonEmpty (y NE.:| ys))
+  = NonEmpty $ x NE.:| (xs ++ y : ys)
 
 
 -- * Stream transformations
 
+-- | Map a function over a NonEmpty stream.
 map :: (a -> b) -> NonEmpty a -> NonEmpty b
 map f = onUnderlying (NE.map f)
 
+-- | Produces a 'NonEmpty' which alternates between elementes of
+-- the input list, and the supplied element.
+--
+-- >>> intersperse 0 [1, 2, 3])
+-- [1, 0, 2, 0, 3]
+--
+-- >>> intersperse 0 [1]
+-- [1]
 intersperse :: a -> NonEmpty a -> NonEmpty a 
 intersperse el = onUnderlying (NE.intersperse el)
 
+-- | scanl is similar to foldl, but returns a stream of successive
+-- reduced values from the left:
+--
+-- >>> scanl (+) 1 [20, 300, 4000]
+-- [1,21,321,4321]
 scanl :: Foldable f => (b -> a -> b) -> b -> f a -> NonEmpty b 
 scanl f initial els = fromNonEmpty $ NE.scanl f initial (Foldable.toList els)
 
+-- | A strict version of 'scanl'.
 scanl' :: Foldable f => (b -> a -> b) -> b -> f a -> NonEmpty b 
 scanl' f initial els = unsafeFromList $ List.scanl' f initial (Foldable.toList els)
 
+-- | Right-to-left dual of scanl. Note that the order of parameters
+-- on the accumulating function are reversed compared to scanl.
+-- Also note that
+--
+-- @
+-- head (scanr f z xs) == foldr f z xs
+-- @
+--
+-- >>> scanr (+) 0 [1..4]
+-- [10,9,7,4,0]
+--
+-- >>> scanr (+) 42 []
+-- [42]
+--
+-- >>> scanr (-) 100 [1..4]
+-- [98,-97,99,-96,100]
 scanr :: Foldable f => (a -> b -> b) -> b -> f a -> NonEmpty b 
 scanr f initial els = fromNonEmpty $ NE.scanr f initial els
 
+-- | scanl1 is a variant of scanl that has no starting value argument:
+--
+-- @
+-- scanl1 f [x1, x2, ...] == [x1, x1 `f` x2, x1 `f` (x2 `f` x3), ...]
+-- @
 scanl1 :: (a -> a -> a) -> NonEmpty a -> NonEmpty a
 scanl1 f = onUnderlying (NE.scanl1 f)
 
+-- | scanr1 is a variant of scanr that has no starting value argument.
+--
+-- >>> scanr1 (+) [1..4]
+-- [10,9,7,4]
+--
+-- >>> scanr1 (+) []
+-- []
+--
+-- >>> scanr1 (-) [1..4]
+-- [-2,3,-1,4]
 scanr1 :: (a -> a -> a) -> NonEmpty a -> NonEmpty a 
 scanr1 f = onUnderlying (NE.scanr1 f)
 
+-- | transpose for NonEmpty, behaves the same as 'List.transpose'.
+-- The rows/columns need not be the same length, in which case
+--
+-- @
+-- transpose . transpose /= id
+-- @
 transpose :: forall a. NonEmpty (NonEmpty a) -> NonEmpty (NonEmpty a)
 transpose = onUnderlying @_ @(NE.NonEmpty (NE.NonEmpty a)) NE.transpose
 
+-- | Behaves the same as 'List.sortBy'
 sortBy :: (a -> a -> Ordering) -> NonEmpty a -> NonEmpty a
 sortBy f = onUnderlying (NE.sortBy f)
 
+-- | Sort a list on a projection of its elements.
+-- Projects once, then sorts, then un-projects.
+-- This is useful when the projection function is expensive.
+-- If it's not, you should probably use 'sortWith'.
 sortOn :: Ord o => (a -> o) -> NonEmpty a -> NonEmpty a 
 sortOn f = onList (List.sortOn f)
 
+-- | Sort a list on a projection of its elements.
+-- Projects during comparison.
+-- This is useful when the projection function is cheap.
+-- If it's not, you should probably use 'sortOn'.
 sortWith :: Ord o => (a -> o) -> NonEmpty a -> NonEmpty a 
 sortWith f = onUnderlying (NE.sortWith f)
 
 
 -- * Building streams
 
+-- | iterate f x produces the infinite sequence of repeated applications of f to x.
+--
+-- @
+-- iterate f x = [x, f x, f (f x), ..]
+-- @
 iterate :: (a -> a) -> a -> NonEmpty a
 iterate f initial = fromNonEmpty $ NE.iterate f initial
 
+-- | repeat x returns a constant stream, where all elements are equal to x.
 repeat :: a -> NonEmpty a
 repeat = fromNonEmpty . NE.repeat
 
+-- | cycle xs returns the infinite repetition of xs:
+--
+-- @
+-- cycle [1, 2, 3] == [1, 2, 3, 1, 2, 3, ...]
+-- @
 cycle :: NonEmpty a -> NonEmpty a
 cycle = onNonEmpty NE.cycle
 
+-- | insert x xs inserts x into the last position in xs where it is still
+-- less than or equal to the next element. If the list is sorted beforehand,
+-- the result will also be sorted.
 insert :: (Foldable f, Ord a) => a -> f a -> NonEmpty a
 insert el = unsafeFromList . List.insert el . Foldable.toList
 
-some1 ::
-  ( Exts.Coercible (f (NE.NonEmpty a)) (f (NonEmpty a))
-  , Alternative f
-  ) => f a -> f (NonEmpty a)
-some1 = Exts.coerce . NE.some1
+-- | some1 x sequences x one or more times.
+some1 :: Alternative f => f a -> f (NonEmpty a)
+some1 = fmap fromNonEmpty . NE.some1
 
 
 -- * Extracting sublists
 
+-- | take n xs returns the first n elements of xs.
 take :: Int -> NonEmpty a -> [a]
 take n = NE.take n . toNonEmpty
 
+-- | drop n xs drops the first n elements from the front of the sequence xs.
 drop :: Int -> NonEmpty a -> [a]
 drop n = NE.drop n . toNonEmpty
 
+-- | splitAt n xs returns a pair consisting of the prefix of xs of length n and the remaining stream immediately following this prefix.
+--
+-- 'splitAt' n xs == ('take' n xs, 'drop' n xs)
 splitAt :: Int -> NonEmpty a -> ([a], [a])
 splitAt n = NE.splitAt n . toNonEmpty
 
+-- | Produces the longest prefix of the stream for which the predicate holds.
 takeWhile :: (a -> Bool) -> NonEmpty a -> [a]
 takeWhile f = NE.takeWhile f . toNonEmpty
 
+-- | dropWhile p xs produces the suffix remaining after takeWhile p xs.
 dropWhile :: (a -> Bool) -> NonEmpty a -> [a]
 dropWhile f = NE.dropWhile f . toNonEmpty
 
+-- | span p xs returns the longest prefix of xs that satisfies p, together with the remainder of the stream.
+--
+-- 'span' p xs == ('takeWhile' p xs, 'dropWhile' p xs)
 span :: (a -> Bool) -> NonEmpty a -> ([a], [a])
 span f = NE.span f . toNonEmpty
 
+-- | break p is equivalent to span (not . p).
 break :: (a -> Bool) -> NonEmpty a -> ([a], [a])
 break f = NE.break f . toNonEmpty
 
+-- | Removes any elements of a nonempty stream that do not satisfy a predicate.
 filter :: (a -> Bool) -> NonEmpty a -> [a]
 filter f = NE.filter f . toNonEmpty
 
+-- | Produces a pair of lists, the first of elements that satisfy the given
+-- predicate, the second of elements that did not.
+--
+-- @
+-- 'partition' p xs == ('filter' p xs, 'filter' (not . p) xs)
+-- @
 partition :: (a -> Bool) -> NonEmpty a -> ([a], [a])
 partition f = NE.partition f . toNonEmpty
 
+-- | Takes a stream and returns a list of streams such that flattening
+-- the resulting list is equal to the argument.
+-- Moreover, each stream in the resulting list contains only equal elements.
+--
+-- >>> group "Mississippi"
+-- ["M", "i", "ss", "i", "ss", "i", "pp", "i"]
 group :: (Foldable f, Eq a) => f a -> [NonEmpty a]
 group = groupBy (==)
 
+-- | Similar to 'group', but sorts the input first so that each
+-- equivalence class has, at most, one list in the output.
+groupAll :: Ord a => [a] -> [NonEmpty a]
+groupAll = groupAllBy compare
+
+-- | Similar to 'group', but uses the provided equality predicate instead of '=='.
 groupBy :: Foldable f => (a -> a -> Bool) -> f a -> [NonEmpty a]
 groupBy f = Exts.coerce . NE.groupBy f
 
+ordToEq :: (a -> b -> Ordering) -> a -> b -> Bool
+ordToEq f a b = f a b == EQ
+
+-- | Similar to 'groupBy', but sorts the input first so that each
+-- equivalence class has, at most, one list in the output.
+groupAllBy :: (a -> a -> Ordering) -> [a] -> [NonEmpty a]
+groupAllBy f = groupBy (ordToEq f) . List.sortBy f
+
+-- | Similar to 'group', but uses the provided projection when comparing for equality.
 groupWith :: (Foldable f, Eq b) => (a -> b) -> f a -> [NonEmpty a]
 groupWith f = Exts.coerce . NE.groupWith f
 
+-- | Similar to 'groupWith', but sorts the input first so that each
+-- equivalence class has, at most, one list in the output.
 groupAllWith :: Ord b => (a -> b) -> [a] -> [NonEmpty a]
 groupAllWith f = Exts.coerce . NE.groupAllWith f
 
+-- | Similar to 'List.group', but uses the knowledge that its input is non-empty to
+-- produce guaranteed non-empty output.
 group1 :: forall a. Eq a => NonEmpty a -> NonEmpty (NonEmpty a)
 group1 = onUnderlying @_ @(NE.NonEmpty (NE.NonEmpty a)) NE.group1
 
+-- | Similar to 'group1', but sorts the input first so that each
+-- equivalence class has, at most, one list in the output.
+groupAll1 :: Ord a => NonEmpty a -> [NonEmpty a]
+groupAll1 = groupBy (==) . sort
+
+-- | Similar to 'group1', but uses the provided equality predicate instead of '=='.
 groupBy1 :: (a -> a -> Bool) -> NonEmpty a -> NonEmpty (NonEmpty a)
 groupBy1 f = onUnderlying (NE.groupBy1 f)
 
+-- | Similar to 'group', but sorts the input first so that each
+-- equivalence class has, at most, one list in the output.
+groupAllBy1 :: (a -> a -> Ordering) -> [a] -> [NonEmpty a]
+groupAllBy1 f = groupBy (ordToEq f) . List.sortBy f
+
+-- | Similar to 'group1', but uses the provided projection when comparing for equality.
 groupWith1 :: Eq b => (a -> b) -> NonEmpty a -> NonEmpty (NonEmpty a)
 groupWith1 f = onUnderlying (NE.groupWith1 f)
 
+-- | Similar to 'groupWith1', but sorts the list first so that each
+-- equivalence class has, at most, one list in the output.
 groupAllWith1 :: Ord b => (a -> b) -> NonEmpty a -> NonEmpty (NonEmpty a)
 groupAllWith1 f = onUnderlying (NE.groupAllWith1 f)
 
